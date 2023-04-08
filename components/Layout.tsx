a98aca715b2a6133a1bd0c9fd2a41e2cc77a5979
Naphat Tisonthi <naphatcs6@gmail.com>
2023-04-08 17:32:53 +0700

    Test 01

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: any) {
 
   return (
     <>
-      <div className='h-screen flex flex-row justify-start bg-gray-100 shadow'>
+      <div className='h-screen flex flex-row justify-start bg-gray-300 shadow'>
         <Sidebar />
         <div className='flex-1 bg-gray-50 overflow-y-auto shadow'>
           <div className="px-4 py-5 sm:px-6 bg-white shadow">
